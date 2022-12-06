8c661c625fd3f61a29d5436d8025f50b8aba21be
MisterConscio <saulo.costam@gmail.com>
2022-12-06 19:43:25 -0300

    Code organization, temp files and new pkg

--- mcarch.sh
+++ mcarch.sh
@@ -1,51 +1,52 @@
 #!/usr/bin/env bash
 
-pkg_list="pkglist.txt"
-aur_list="aurlist.txt"
-
 bold=$(tput bold)
 normal=$(tput sgr0)
 
 error() {
   echo -e "\n${bold}${1:-Ocorreu algum erro}${normal}\n"
-  sleep 2
+  sleep 1
   exit 1
 }
 
 message() {
   echo -e "\n${bold}$1${normal}\n" >&2
-  sleep 1
+  sleep 0.5
 }
 
 hello() {
   clear
   echo -e "\n${bold}Bem vindo${normal}\n"
-  sleep 2
   echo "Irá começar o script de instalação"
-  sleep 2
   echo "Esse script é destinado para sistemas ${bold}Arch Linux${normal}"
-  sleep 2
+
   read -rp "Antes de começar, por farvor ${bold}informe seu usuário${normal}: " name
   [ ! "$(id -u "$name")" ] && error "O usuário ${name} não existe"
+
   read -rp "Por farvor, ${bold}informe qual é sua placa de vídeo${normal} [nvidia/intel/amd]: " video
   echo "${bold}Vamos-lá ${name} :)${normal}"
+
   sleep 1
 }
 
 mkfilestruct() {
   message "Estrutura de arquivos"
+
   sudo -u "$name" mkdir -pv /home/"$name"/.config/{mpd,ncmpcpp,zsh} \
     /home/"$name"/.cache/zsh \
     /home/"$name"/.local/{src,state,share/{npm,backgrounds}} \
     /home/"$name"/media/{pic/screenshot,vid,mus,samp,proj,emu} \
     /home/"$name"/{dev,doc}
+
   mkdir -pv /mnt/{externo,ssd,usb1,usb2,usb3}
   cd /mnt && chown -v -R "$name":"$name" ./*
+
   message "Finalizada"
 }
 
 setpacman() {
   message "Configuração do pacman e sudoers"
+
   pacman --noconfirm --needed -S pacman-contrib
 
   sed -E -i "s/^#(ParallelDownloads).*/\1 = 5/;/^#Color$/s/#//;/^#VerbosePkgLists$/s/#//;/\[multilib\]/,/Include/s/#//" /etc/pacman.conf
@@ -60,85 +61,105 @@ setpacman() {
 }
 
 dotfiles() {
-  dotfiles_repo="https://github.com/MisterConscio/dotfiles.git"
   message "Repositório dos dotfiles"
+
+  dotfiles_repo="https://github.com/MisterConscio/dotfiles.git"
   dotdir="/home/$name/dotfiles"
+
   pacman --noconfirm --needed -S stow git
-  echo -e "\nClonando o repositório dos dotfiles..."
+
   sudo -u "$name" git clone "$dotfiles_repo" "$dotdir"
   cd "$dotdir" || error "cd failed"
   sudo -u "$name" stow -v */
+
   message "Finalizada"
 }
 
 pacinstall() {
   message "Instalação de programas"
+
+  pkg_list="/tmp/pkglist.txt"
+  aur_list="/tmp/aurlist.txt"
+
   cd /home/"$name" || error "cd failed"
-  curl -LO "https://raw.githubusercontent.com/MisterConscio/MCARCH/main/pkglist.txt"
-  curl -LO "https://raw.githubusercontent.com/MisterConscio/MCARCH/main/aurlist.txt"
-  [ ! -e "/home/$name/$pkg_list" ] && error "O arquivo $pkg_list não existe"
-  echo "${bold}Iniciando a instalação...${normal}"
+
+  curl -L "https://raw.githubusercontent.com/MisterConscio/MCARCH/main/pkglist.txt" -o "$pkg_list"
+  curl -L "https://raw.githubusercontent.com/MisterConscio/MCARCH/main/aurlist.txt" -o "$aur_list"
+
+  [ ! -f "$pkg_list" ] && error "O arquivo $pkg_list não existe"
+
   pacman --noconfirm --needed -S - < "$pkg_list"
+
   case "$video" in
     intel) pacman --noconfirm --needed -S xf86-video-intel lib32-mesa;;
     amd) pacman --noconfirm --needed -S xf86-video-amdgpu xf86-video-ati lib32-mesa;;
     nvidia) pacman --noconfirm --needed -S nvidia nvidia-utils lib32-nvidia-utils;;
     *) echo "Placa de vídeo incorreta ou não especificada";;
   esac
+
   message "Finalizada"
 }
 
 aurinstall() {
+  message "Instalação do Yay"
+
   aurhelper="yay"
   aurhelper_git="https://aur.archlinux.org/yay.git"
-  message "Instalação do Yay"
-  echo "Instalando ${aurhelper} como AUR helper..."
   aurdir="/home/$name/.local/src/$aurhelper"
+
   sudo -u "$name" git clone "$aurhelper_git" "$aurdir"
+
   cd "$aurdir" || error "cd failed"
+
   sudo -u "$name" makepkg -sirc --noconfirm || error
+
   message "Finalizada"
 }
 
 aurpkg() {
   message "Instalação de pacotes AUR"
-  echo "Instalando pacotes do AUR..."
+
   cd /home/"$name" || error "cd failed"
-  [ ! -e "/home/$name/$aur_list" ] && error "O arquivo $aur_list não existe"
-  # cd "$dotdir"
+
+  [ ! -f "$aur_list" ] && error "O arquivo $aur_list não existe"
+
   sudo -u "$name" yay -S --removemake --noconfirm - < "$aur_list"
+
   message "Finalizada"
 }
 
 vimplug() {
   message "Instalação dos plugins do vim"
+
   sudo -u "$name" mkdir -pv /home/"$name"/.local/share/nvim/site/autoload
+
   curl -Ls \
     "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" > \
     "/home/$name/.local/share/nvim/site/autoload/plug.vim"
+
   sudo -u "$name" nvim -c "PlugInstall|q|q"
+
   message "Finalizada"
 }
 
-changeshell() {
+final_setup() {
   message "Mundança de shell para zsh"
-  echo "Mudando o shell para zsh..."
+
   chsh -s /usr/bin/zsh "$name"
   chsh -s /usr/bin/zsh root
-  message "Finalizada"
-}
 
-addgroups() {
+  # Adição de grupos ao usuário
   message "Adcionando ao usuário grupos"
+
   usermod -aG wheel,video,audio,lp,network,kvm,storage,i2c "$name"
   echo "command: usermod -aG wheel,video,audio,lp,network,kvm,storage,i2c $name"
-  message "Finalizada"
-}
 
-cleanup() {
+  # Cleanup
   message "Limpeza"
-  rm -rfv /home/"${name:?}"/{mcarch.sh,"${pkg_list}","${aur_list}",.bash_logout,.bashrc,.bash_profile,go}
-  mv -v /home/"${name:?}"/.gnupg /home/"$name"/.local/share/gnupg
+
+  rm -rfv /home/"$name"/.bash* /home/"$name"/.go
+  mv -v /home/"$name"/.gnupg /home/"$name"/.local/share/gnupg
+
   message "Finalizada"
 }
 
@@ -172,14 +193,8 @@ aurpkg || error
 # Vimplug install
 # vimplug || error
 
-# Mudança de shell para zsh
-changeshell || error
-
-# Gerenciamento de grupos
-addgroups || error
-
-# Limpeza
-cleanup || error
+# Últimas configurações
+final_setup || error
 
 # Configuração do servidor de áudio Jack para uso do Realtime Scheduling
 [ ! -e /etc/security/limits.d/00-audio.conf ] &&
@@ -205,7 +220,7 @@ EOF
 
 echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/00-sudo-wheel
 echo -e "Defaults timestamp_timeout=30\nDefaults timestamp_type=global" > /etc/sudoers.d/01-sudo-time
-echo "%wheel ALL=(ALL:ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/mount,/usr/bin/umount" > /etc/sudoers.d/02-cmd-nopasswd
+echo "%wheel ALL=(ALL:ALL) NOPASSWD: /usr/bin/poweroff,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/mount,/usr/bin/umount" > /etc/sudoers.d/02-cmd-nopasswd
 echo "Defaults editor=/usr/bin/nvim" > /etc/sudoers.d/03-visudo-editor
 
 echo "PROMPT='%F{red}%B%1~%b%f %(!.#.>>) '" > /root/.zshrc
